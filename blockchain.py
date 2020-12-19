e094d8d947f1a64d43be00466a9576f520dd9868
PratikGarai <pratikgarai0208@gmail.com>
2020-12-19 06:22:10 +0530

    Description added too

--- blockchain.py
+++ blockchain.py
@@ -1,27 +1,51 @@
+import time
+import json
+import hashlib
+
 class BlockChain(object):
 
     def __init__(self, *args, **kwargs):
         self.chain = []
-        self.current_transaction = []
+        self.current_transactions = []
+
+        # adding genesis
+        self.new_block(100, 1)
+        
+    
+    # create a new block and  add to chain
+    def new_block(self, proof, previous_hash = None):
+        block = {
+                'index' : len(self.chain)+1,
+                'timestamp' : time.time_ns(),
+                'transactions' : self.current_transactions,
+                'proof' : proof,
+                'previous_hash' : previous_hash
+            }
+
+        self.current_transactions=  []
+        self.chain.append(block)
+        return block
 
     
-    def new_block(self):
-        # Create a new block and add to chain
-        pass
+    # Add a new transaction to the current transaction list
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'sender' : sender,
+            'recipient' : recipient,
+            'amount' : amount
+            })
+
+        return self.last_block['index']+1
     
 
-    def new_transaction(self):
-        # Add a new transaction to the current transaction list
-        pass
-
-    
+    # Calculate Hash of a block
     @staticmethod
     def hash(block):
-        # Calculate Hash of a block
-        pass
-    
+        block_string = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
+
+    # Return the last block in the chain
     @property
     def last_block():
-        # Return the last block in the chain
-        pass
+        return self.chain[-1]
