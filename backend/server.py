9299e10fe38904466357b0dafa1266e9971a34de
PratikGarai <pratikgarai0208@gmail.com>
2021-01-26 23:10:43 +0530

    completed th method implementations

--- backend/server.py
+++ backend/server.py
@@ -5,24 +5,53 @@ from time import time
 from uuid import uuid4
 
 import flask
+from flask import jsonify, Flask, request
 
 from blockchain import BlockChain
 
-app = flask.Flask(__name__)
-node_ideantifier = str(uuid4()).replace('-','')
+app = Flask(__name__)
+node_identifier = str(uuid4()).replace('-','')
 blockchain = BlockChain()
 
 
 # This endpoint is to mine a new block
 @app.route('/mine', methods=['GET'])
 def mine():
-    return ""
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+
+    return jsonify(response), 200
 
 
 # This endpoint creates new transaction
 @app.route('/transaction/new', methods=['POST'])
 def new_transaction():
-    return ""
+    values = request.get_json()
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
 
 
 # This endpoint returns the whole chain
@@ -32,7 +61,7 @@ def full_chain():
         'chain': blockchain.chain,
         'length': len(blockchain.chain),
     }
-    return flask.jsonify(response), 200
+    return jsonify(response), 200
 
 
 if __name__ == '__main__':
