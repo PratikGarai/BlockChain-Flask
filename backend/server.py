98a1359ff3e06491b9593175986b2ef1bef01045
PratikGarai <pratikgarai0208@gmail.com>
2021-01-26 22:39:34 +0530

    initialized frontend with a wrong name  😅

--- backend/server.py
+++ backend/server.py
@@ -12,14 +12,20 @@ app = flask.Flask(__name__)
 node_ideantifier = str(uuid4()).replace('-','')
 blockchain = BlockChain()
 
+
+# This endpoint is to mine a new block
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "This endpoint is to mine a new block"
+    return ""
 
+
+# This endpoint creates new transaction
 @app.route('/transaction/new', methods=['POST'])
 def new_transaction():
-    return "This endpoint creates new transaction"
+    return ""
 
+
+# This endpoint returns the whole chain
 @app.route('/chain', methods=['GET'])
 def full_chain():
     response = {
@@ -27,6 +33,7 @@ def full_chain():
         'length': len(blockchain.chain),
     }
     return flask.jsonify(response), 200
+
 
 if __name__ == '__main__':
     app.run(port=5000)
